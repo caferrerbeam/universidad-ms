5f78dc2c32ae0c91dc738bb307c86e629a6405e6
Jhon Quirama <quirama80@gmail.com>
2019-09-18 15:06:38 -0500

    [resolves #1] crud programs ready

---
+++ src/main/java/co/edu/eam/ingesoft/products_ms/controllers/ProgramController.java
@@ -0,0 +1,117 @@
+package co.edu.eam.ingesoft.products_ms.controllers;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.eam.ingesoft.products_ms.model.Program;
+import co.edu.eam.ingesoft.products_ms.routes.Router;
+import co.edu.eam.ingesoft.products_ms.services.ProgramService;
+
+@RestController
+@RequestMapping(Router.PROGRAMS_PATH)
+public class ProgramController {
+	/**
+	 * program service.
+	 */
+	@Autowired
+	private ProgramService programService;
+
+	/**
+	 * create a program operation.
+	 *
+	 * @param program program to create
+	 * @return program created
+	 */
+	@PostMapping(value = Router.CREATE_PROGRAMS)
+	public Program create(@RequestBody Program program) {
+		return programService.create(program);
+	}
+
+	/**
+	 * find a program.
+	 *
+	 * @param id       id for program to find
+	 * @param response httpResponse
+	 * @return program with id
+	 */
+	@GetMapping(value = Router.FIND_PROGRAMS + "/{id}")
+	public Program find(@PathVariable Integer id, HttpServletResponse response) {
+		Program program = programService.find(id);
+
+		if (program == null) {
+			response.setStatus(HttpStatus.NOT_FOUND.value());
+		}
+
+		return program;
+	}
+
+	/**
+	 * Delete a program.
+	 *
+	 * @param id id program to delete
+	 * @return program deleted
+	 */
+	@DeleteMapping(value = Router.DELETE_PROGRAMS+ "/{id}")
+	public Program delete(@PathVariable Integer id) {
+		return programService.delete(id);
+	}
+
+	/**
+	 * Edit a program.
+	 *
+	 * @param program program to edit
+	 * @return program edited
+	 */
+	@PutMapping(value = Router.EDIT_PROGRAMS)
+	public Program edit(@RequestBody Program program) {
+		return programService.update(program);
+	}
+
+	/**
+	 * find a program by name.
+	 *
+	 * @param program program program to find
+	 * @return list of program with a name
+	 */
+	@GetMapping(value = Router.FIND_BY_NAME)
+	public ResponseEntity<List<Program>> findByName(@RequestParam String name) {
+		List<Program> programs = programService.findByName(name);
+
+		if (programs.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+
+		return new ResponseEntity<>(programs, HttpStatus.OK);
+	}
+
+	/**
+	 * list all programs.
+	 *
+	 * @param response httpResponse
+	 * @return list of all programs
+	 */
+	@GetMapping(value = Router.FIND_ALL)
+	public List<Program> findAll(HttpServletResponse response) {
+		List<Program> programs = programService.listAll();
+
+		if (programs.isEmpty()) {
+			response.setStatus(HttpStatus.NO_CONTENT.value());
+		}
+
+		return programs;
+	}
+}
---
+++ src/main/resources/db/migration/V2.0__programs.sql
@@ -0,0 +1,6 @@
+CREATE TABLE public.programs
+(
+    id       integer PRIMARY KEY,
+    name     VARCHAR(50) NOT NULL,
+    facultad_id integer NOT NULL
+);
